9a2db405db3104e4e8a17ffd50cdaa8d987056a5
Hasib <hasibulhasan2048@gmail.com>
2025-06-29 18:56:33 +0600

    Registation logic

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,39 @@
+import User from "@/models/User";
+import { connectToDatabase } from "@/utils/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST (Request: NextRequest){
+    try {
+        const {email, password} = await Request.json()
+        if (!email || !password) {
+            return NextResponse.json(
+                {error: "Email and Password are required"},
+                {status: 400}
+            )
+        }
+
+        await connectToDatabase()
+        const existingUser = await User.findOne({email})
+        if (existingUser) {
+            return NextResponse.json(
+                {error: "User already exists"},
+                {status: 400}
+            )
+        }
+        await User.create({
+            email, password
+        })
+
+        return NextResponse.json(
+            {message: "User registered Successfully"}
+            {status: 400}
+        )
+
+    } catch (error) {
+        console.error("Registration error", error)
+        NextResponse.json(
+            {error: "Regestration Failed"}
+            {status: 400}
+        )
+    }
+}
